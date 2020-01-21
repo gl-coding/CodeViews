c8d653496c9046c969ee705612c4cd3e32c0e19b
gl-coding <1451607278@qq.com>
2020-01-21 15:58:58 +0800

    format

--- spider/pick_leetcode.py
+++ spider/pick_leetcode.py
@@ -54,25 +54,82 @@ def getProblemListPage(url):
     #完成，退出
     driver.quit()
 
-def detailPick(filename, url):
-    html = urllib.urlopen(url).read()
-    with open(filename, "w+") as f:
-        f.write(html)
+def detailPick(filename):
+    res = []
+    res_item = []
+    cate_dic = {}
+    sim_dic  = []
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith("path:"):
+                #print res_item
+                if len(res_item) != 0:
+                    res.append(res_item)
 
-def contentPick(path):
-    htmlfile = open(path, 'r')
-    soup = BeautifulSoup(htmlfile, 'lxml')
-    entrys = soup.find_all('div', id='cnblogs_post_body')
-    title = soup.find('a', class_='postTitle2')
-    return title.string, entrys[0].text
+                flag, ret = categoryPick(res_item)
+                if flag != None:
+                    #print "\t".join(ret)
+                    flag = int(flag)
+                    for item in ret:
+                        if item not in cate_dic:
+                            cate_dic[item] = set([flag])
+                        else:
+                            cate_dic[item].add(flag)
 
-def get_link(string, idx):
-    pre = ""
-    for line in string.split("\n"):
-        line = line.strip()
-        if pre.startswith(u"参考"):
-            print idx, line
-        pre = line
+
+                flag, ret = simPick(res_item)
+                if flag != None:
+                    #flag = int(flag)
+                    #print "\t".join(ret)
+                    sim_dic.append(ret)
+
+                #print "\t".join(ret)
+                res_item = []
+            res_item.append(line)
+    return cate_dic, sim_dic
+
+def categoryPick(data_list):
+    idx = data_list[0].replace("path:leet/", "").replace(".html", "")
+    if not idx.isdigit():
+        return None, []
+    res = []
+    flag = 0
+    for item in data_list:
+        if "相关标签" in item:
+            res = []
+            flag = 1
+            continue
+        if "相似题目" in item or "显示提示" in item or "题目列表" in item:
+            break
+        res.append(item.strip())
+    if flag == 1:
+        #print idx
+        return idx, res
+    else:
+        return None, []
+
+def simPick(data_list):
+    idx = data_list[0].replace("path:leet/", "").replace(".html", "")
+    if not idx.isdigit():
+        return None, []
+    res = []
+    flag = 0
+    for item in data_list:
+        if "相似题目" in item:
+            res = []
+            flag = 1
+            continue
+        if "显示提示" in item or "题目列表" in item:
+            break
+        if "简单" in item or "中等" in item or "困难" in item:
+            continue
+        else:
+            res.append(item.strip())
+    if flag == 1:
+        return idx, res
+    else:
+        return None, []
 
 def getPageDetail(filename, begin_idx=0):
     #driver = webdriver.Chrome()
@@ -126,6 +183,48 @@ def getPageDetail(filename, begin_idx=0):
             #break
     #driver.quit()
 
+num_dic = {
+        1: "一",
+        2: "二",
+        3: "三",
+        4: "四",
+        5: "五",
+        6: "六",
+        7: "七",
+        8: "八",
+        9: "九",
+        10:"十",
+        11:"十一",
+        12:"十二",
+        13:"十三",
+        14:"十四",
+        15:"十五",
+        16:"十六",
+        17:"十七",
+        18:"十八",
+        19:"十九",
+        20:"二十",
+        21:"二一",
+        22:"二二",
+        23:"二三",
+        24:"二四",
+        25:"二五",
+        26:"二六",
+        27:"二七",
+        28:"二八",
+        29:"二九",
+        30:"三十",
+        31:"三一",
+        32:"三二",
+        33:"三三",
+        34:"三四",
+        35:"三五",
+        36:"三六",
+        37:"三七",
+        38:"三八",
+        39:"三九"
+        }
+
 if __name__ == '__main__':
     begin_url = "https://leetcode-cn.com/problemset/all/"
 
@@ -135,5 +234,41 @@ if __name__ == '__main__':
     #save problem html page
     #getPageDetail("problem.idx")
 
-    getPageDetail("leetcode.idx", 204)
+    #getPageDetail("leetcode.idx", 204)
 
+    cate_dic, sim_dic = detailPick("leet.log")
+
+    print len(cate_dic)
+    gcounter = 0
+    print "* Summary\n"
+    print "* [序](Abstract.md)"
+    print "* [前言](README.md)"
+    for k, v in cate_dic.items():
+        gcounter += 1
+        ch_counter = num_dic[gcounter]
+        print "* [" + ch_counter + "、" + k + "](Chapter" + str(gcounter) +  "/README.md)"
+
+        v = list(v)
+        v = sorted(v)
+        #print v
+
+        res = []
+        item_res = []
+        counter = 0
+        chapter = 1
+        for item in v:
+            counter += 1
+            #print counter
+            item_res.append(item)
+            if counter == len(v):
+                print "    * [第" + str(chapter) + "节](Chapter" + str(gcounter) + "/" + str(counter) + ".md)"
+                print "\n".join([str(i) for i in item_res])
+                res.append(item_res)
+            if counter % 12 == 0:
+                print "    * [第" + str(chapter) + "节](Chapter" + str(gcounter) + "/" + str(counter) + ".md)"
+                chapter += 1
+                print "\n".join([str(i) for i in item_res])
+                res.append(item_res)
+                item_res = []
+            
+            
